9e61f574770cde5b16101c60dfbb946552d30094
yameenxd <eng23cs0367@dsu.edu.in>
2025-08-27 17:16:33 +0530

    numbers from 1 to 10

---
+++ loop2.java
@@ -0,0 +1,9 @@
+//print the numbers from 0 to 10
+import java.util.*;
+public class solution{
+  public static void main(String[] args) {
+      for (int i = 0;i < 11;i++) {
+        System.out.println("printing the first 10 numbers:"+i);
+      }
+  }
+}
