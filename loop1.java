51141069a9b7f49f128b8f1319730e6a12f5a4bc
yameenxd <eng23cs0367@dsu.edu.in>
2025-08-27 17:25:47 +0530

    updated for loop

---
+++ loop1.java
@@ -0,0 +1,9 @@
+// PRINT HELLO WORLD USING FOR LOOP CONDITION
+import java.util.*;
+public class solutions{
+    public static void main(String[]args){
+        for(int i =0; i<3; i++){
+            System.out.println("hello world");
+        }
+    }
+}
