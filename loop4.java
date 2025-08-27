b8de7260914a9faae899ca2624e8974eb9d8e270
yameenxd <eng23cs0367@dsu.edu.in>
2025-08-27 18:31:46 +0530

    using while loop

---
+++ loop4.java
@@ -0,0 +1,12 @@
+//number from 1 to 10 using do while loop 
+import java.util.*;
+public class solution{
+  public static void main(String[] args) {
+      int i = 0;
+      do{
+          System.out.println(i);
+          i++;
+      }
+      while(i<11);
+  }
+}
