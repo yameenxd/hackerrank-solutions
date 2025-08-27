d05a3c132259040b3deabce5e7f1562b3b646cd1
yameenxd <eng23cs0367@dsu.edu.in>
2025-08-27 19:03:38 +0530

    sum of n natural numbers

---
+++ sumNnatural.java
@@ -0,0 +1,14 @@
+//PRINT THE SUM OF N NATURAL NUMBERS 
+import java.util.*;
+public class solution{
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println("insert the n numbers to be added:"+n);
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum = sum + i;
+        }
+        System.out.println("soultion of n natural numbers:"+sum);
+    }
+}
