2cc56c81f8e04e67e8146293774523455291807e
yameenxd <eng23cs0367@dsu.edu.in>
2025-08-29 12:03:32 +0530

    basic addition

---
+++ addition.java
@@ -0,0 +1,11 @@
+// sum of the two numbers 
+import java.util.*;
+public class Solution {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
