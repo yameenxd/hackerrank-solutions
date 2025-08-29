1d10f5258503fd1d17abc2376f15ab3e9a5c5780
yameenxd <eng23cs0367@dsu.edu.in>
2025-08-29 11:26:17 +0530

    updated multiplication code

--- multiplication.java
+++ multiplication.java
@@ -1,1 +1,13 @@
-// MULTIPLICATION IN JAVA 
+/*Given an integer,n, print its first 10 multiples. Each multiple n x i (where 1<i<10)
+ should be printed on a new line in the form: N x i = result.*/
+
+ import java.util.*;
+public class solution{
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i=1;i<11;i++){
+            System.out.println(n+" x "+i+" = "+n*i); 
+        }
+    }
+}
